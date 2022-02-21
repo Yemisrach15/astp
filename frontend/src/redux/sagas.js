62b9f7b365a30207fb93340178d334e4cf563415
Yemisrach <se.yemisrach.serawit@gmail.com>
2022-02-21 10:01:02 +0300

    Add sagas for getOne, update and remove

--- frontend/src/redux/sagas.js
+++ frontend/src/redux/sagas.js
@@ -1,10 +1,6 @@
 import { takeEvery, call, put, all } from 'redux-saga/effects';
-import { getAllEmployees, createEmployee } from '../server/api';
+import { getAllEmployees, getEmployee, postEmployee, putEmployee, deleteEmployee } from '../server/api';
 import ActionTypes from './ActionTypes';
-
-function* watchGetEmployees() {
-    yield takeEvery(ActionTypes.GET_EMPLOYEES, getEmployees);
-}
 
 function* getEmployees() {
     try {
@@ -16,13 +12,19 @@ function* getEmployees() {
     }
 }
 
-function* watchCreateEmployee() {
-    yield takeEvery(ActionTypes.ADD_NEW_EMPLOYEE, newEmployee);
+function* getOneEmployee(action) {
+    try {
+        const res = yield call(getEmployee, action.payload._id);
+        yield put({ type: ActionTypes.GET_EMPLOYEE_SUCCESS, payload: res.data });
+    }
+    catch {
+        yield put({ type: ActionTypes.GET_EMPLOYEE_FAIL });
+    }
 }
 
 function* newEmployee(action) {
     try {
-        const res = yield call(createEmployee, action.payload);
+        const res = yield call(postEmployee, action.payload);
         yield put({ type: ActionTypes.ADD_NEW_EMPLOYEE_SUCCESS, payload: res.data });
     }
     catch {
@@ -30,9 +32,52 @@ function* newEmployee(action) {
     }
 }
 
+function* updateEmployee(action) {
+    try {
+        const res = yield call(putEmployee, action.payload._id, action.payload);
+        yield put({ type: ActionTypes.UPDATE_SALARY_SUCCESS, payload: res.data });
+    }
+    catch {
+        yield put({ type: ActionTypes.UPDATE_SALARY_FAIL })
+    }
+}
+
+function* removeEmployee(action) {
+    try {
+        const res = yield call(deleteEmployee, action.payload._id);
+        yield put({ type: ActionTypes.DELETE_EMPLOYEE_SUCCESS, payload: { _id: action.payload._id } });
+    }
+    catch {
+        yield put({ type: ActionTypes.DELETE_EMPLOYEE_FAIL });
+    }
+}
+
+function* watchGetEmployees() {
+    yield takeEvery(ActionTypes.GET_EMPLOYEES, getEmployees);
+}
+
+function* watchGetOneEmployee() {
+    yield takeEvery(ActionTypes.GET_EMPLOYEE, getOneEmployee);
+}
+
+function* watchCreateEmployee() {
+    yield takeEvery(ActionTypes.ADD_NEW_EMPLOYEE, newEmployee);
+}
+
+function* watchUpdateEmployee() {
+    yield takeEvery(ActionTypes.UPDATE_SALARY, updateEmployee);
+}
+
+function* watchRemoveEmployee() {
+    yield takeEvery(ActionTypes.DELETE_EMPLOYEE, removeEmployee);
+}
+
 export default function* rootSaga() {
     yield all([
         watchGetEmployees(),
-        watchCreateEmployee()
+        watchGetOneEmployee(),
+        watchCreateEmployee(),
+        watchUpdateEmployee(),
+        watchRemoveEmployee()
     ])
 }
--- frontend/src/server/api.js
+++ frontend/src/server/api.js
@@ -4,6 +4,6 @@ axios.defaults.baseURL = "http://localhost:9000/api/v1";
 
 export const getAllEmployees = async () => axios.get("/employees");
 export const getEmployee = async (id) => axios.get(`/employees/${id}`);
-export const createEmployee = async (employee) => axios.post("/employees", employee);
-export const updateEmployee = async (id, employee) => axios.post(`/employees/${id}`, employee);
+export const postEmployee = async (employee) => axios.post("/employees", employee);
+export const putEmployee = async (id, employee) => axios.put(`/employees/${id}`, {...employee, birthDate: new Date(employee.birthDate) });
 export const deleteEmployee = async (id) => axios.delete(`/employees/${id}`);
