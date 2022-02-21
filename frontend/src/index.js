b0ac6df7668b31dc6e04e85788d6cd50eb36f6e6
Yemisrach <se.yemisrach.serawit@gmail.com>
2022-02-21 11:45:32 +0300

    Persist redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,28 +5,39 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { devToolsEnhancer } from 'redux-devtools-extension';
 import reducer from './redux/reducer';
 import ActionTypes from './redux/ActionTypes';
 import rootSaga from './redux/sagas';
 
+const persistConfig = {
+  key: 'employees',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(reducer, compose(applyMiddleware(sagaMiddleware), devToolsEnhancer({ trace: true })));
+const store = createStore(persistedReducer, compose(applyMiddleware(sagaMiddleware), devToolsEnhancer({ trace: true })));
+const persistedStore = persistStore(store);
 sagaMiddleware.run(rootSaga);
 
 const action = (type, payload) => store.dispatch({ type, payload });
-action(ActionTypes.GET_EMPLOYEES);
 
 function render() {
   ReactDOM.render(
     <React.StrictMode>
-        <App 
+      <PersistGate persistor={persistedStore}>
+        <App
           state={store.getState()}
-          onInit={(payload) => action(ActionTypes.GET_EMPLOYEES, payload)}
+          onInit={() => action(ActionTypes.GET_EMPLOYEES)}
           onGetOne={(payload) => action(ActionTypes.GET_EMPLOYEE, payload)}
           onCreate={(payload) => action(ActionTypes.ADD_NEW_EMPLOYEE, payload)}
           onEdit={(payload) => action(ActionTypes.UPDATE_SALARY, payload)}
           onDelete={(payload) => action(ActionTypes.DELETE_EMPLOYEE, payload)} />
+      </PersistGate>
     </React.StrictMode>,
     document.getElementById('root')
   )
